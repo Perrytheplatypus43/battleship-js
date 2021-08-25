31bf5233efd70860ebfd37e4f4aaf5b7b3f6ae00
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2021-08-25 13:24:49 -0700

    added ship array

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ document.addEventListener('DomContentLoaded', () => {
 
 
     //Create Boards
-    function createBoard(grid, squares, width) {
+    function createBoard(grid, squares) {
         for (let i=0; i < width * width; i++) {
             const square = document.createElement('div')
             square.dataset.id = i
@@ -25,7 +25,45 @@ document.addEventListener('DomContentLoaded', () => {
             squares.push(square)
         }
     }
+    createBoard(userGrid, userSquares)
+    createBoard(computerGrid, computerSquares)
 
-    createBoard(userGrid, userSquares, width)
-    createBoard(computerGrid, computerSquares, width)
+    //Ships
+    const shipArray = [
+        {
+            name: 'destroyer',
+            directions: [
+                [0, 1],
+                [0, width]
+            ]
+        },
+        {
+            name: 'submarine',
+            directions: [
+                [0, 1, 2],
+                [0, width, width*2]
+            ]
+        },
+        {
+            name: 'cruiser',
+            directions: [
+                [0, 1, 2],
+                [0, width, width*2]
+            ]
+        },
+        {
+            name: 'battleship',
+            directions: [
+                [0, 1, 2, 3],
+                [0, width, width*2, width*3]
+            ]
+        },
+        {
+            name: 'carrier',
+            directions: [
+                [0, 1, 2, 3, 4],
+                [0, width, width*2, width*3, width*4]
+            ]
+        }
+    ]
 })
