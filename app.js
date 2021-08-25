b72a806450d21309fea5c6e4926486c9a55dab03
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2021-08-25 13:00:49 -0700

    geting all ship div id's and puting it in const's

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 document.addEventListener('DomContentLoaded', () => {
-    
+    const userGrid = document.querySelector('.grid-user')
+    const computerGrid = document.querySelector('.grid-computer')
+    const displayGrid = document.querySelector('.grid-display')
+    const ship = document.querySelectorAll('.ship')
+    const destroyer = document.querySelector('.destroyer-container')
+    const submarine = document.querySelector('.submarine-container')
+    const cruiser = document.querySelector('.cruiser-container')
+    const battleship = document.querySelector('.battleship-container')
+    const carrier = document.querySelector('.carrier-container')
 })
