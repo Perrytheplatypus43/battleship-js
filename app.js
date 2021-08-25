7d90a66ef47950570a1eddc853f7546da0ec7bf9
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2021-08-25 14:51:48 -0700

    Made generate function

--- app.js
+++ app.js
@@ -66,4 +66,21 @@ document.addEventListener('DomContentLoaded', () => {
             ]
         }
     ]
+
+    // Draw the computers ships at random
+    function generate() {
+        let randomDirection = Math.floor(Math.random() * ship.directions.length)
+        let current = ship.directions[randomDirection]
+        if (randomDirection === 0) direction = 1
+        if (randomDirection === 1) direction = 10
+        let randomStart = Math.floor(Math.random() * computerSquares.length - (ship.directions[0].length * direction))
+
+        const isTaken = current.some(index => computerSquares[randomStart + index].classList.contains('taken'))
+        const isAtRightEdge = current.some(index => (randomStart + index) % width === width - 1)
+        const isAtLeftEdge = current.come(index => (randomStart + index) % width === 0)
+
+        if (!isTaken && !isAtRightEdge && !isAtLeftEdge) current.forEach(index => computerSquares[randomStart + index].classList.add('taken', ship.name))
+
+        else generate(ship)
+    }
 })
