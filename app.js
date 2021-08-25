a43e176a43a5da7c6f85fde9c59a787815f9e325
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2021-08-25 16:47:27 -0700

    fixing more typos

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-document.addEventListener('DomContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
     const userGrid = document.querySelector('.grid-user')
     const computerGrid = document.querySelector('.grid-computer')
     const displayGrid = document.querySelector('.grid-display')
@@ -20,7 +20,7 @@ document.addEventListener('DomContentLoaded', () => {
 
     //Create Boards
     function createBoard(grid, squares) {
-        for (let i=0; i < width * width; i++) {
+        for (let i = 0; i < width*width; i++) {
             const square = document.createElement('div')
             square.dataset.id = i
             grid.appendChild(square)
